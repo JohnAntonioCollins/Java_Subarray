1242d0796829028051bfbf7aab6c7fbe018f7832
JohnAntonioCollins <john@johncollins.ws>
2017-05-03 20:11:47 -0400

    first commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,18 @@
+/**
+ * @Author John Antonio Collins
+ * Created on 5/3/17.
+ * this is the Solution class that I copy/pasted into hackerrank.com
+ */
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        //MY CODE START
+
+        //MY CODE END
+
+    }
+}
