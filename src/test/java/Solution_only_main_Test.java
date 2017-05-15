1483e6e0bf516ab4332eb774d8a12ddbabcea435
JohnAntonioCollins <john@johncollins.ws>
2017-05-15 00:58:39 -0400

    fixed Solution_only_main_main_Test by adding System.setOut to grab console output

--- src/test/java/Solution_only_main_Test.java
+++ src/test/java/Solution_only_main_Test.java
@@ -2,6 +2,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @Author John Antonio Collins
@@ -22,6 +26,11 @@ public class Solution_only_main_Test
     public void Solution_only_main_main_Test(){
         ByteArrayInputStream simIn = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(simIn);
+        ByteArrayOutputStream getOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(getOut));
         solution.main(null);
+        String actual = getOut.toString().trim();
+        String expected = "9";
+        assertEquals(expected, actual);
     }
 }
